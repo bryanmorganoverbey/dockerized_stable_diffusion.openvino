35c546d5043c8f353aa36e436b431a0745c56e71
Bryan Overbey <bryanmorganoverbey@gmail.com>
2023-11-03 18:27:18 -0500

    keep the python script running, asking for input

--- demo.py
+++ demo.py
@@ -13,7 +13,7 @@ import numpy as np
 from openvino.runtime import Core
 
 
-def main(args):
+def run_prompt(args):
     if args.seed is None:
         args.seed = random.randint(0, 2**30)
     np.random.seed(args.seed)
@@ -29,7 +29,7 @@ def main(args):
             beta_start=args.beta_start,
             beta_end=args.beta_end,
             beta_schedule=args.beta_schedule,
-            skip_prk_steps = True,
+            skip_prk_steps=True,
             tensor_format="np"
         )
     engine = StableDiffusionEngine(
@@ -40,7 +40,8 @@ def main(args):
     )
     image = engine(
         prompt=args.prompt,
-        init_image=None if args.init_image is None else cv2.imread(args.init_image),
+        init_image=None if args.init_image is None else cv2.imread(
+            args.init_image),
         mask=None if args.mask is None else cv2.imread(args.mask, 0),
         strength=args.strength,
         num_inference_steps=args.num_inference_steps,
@@ -50,34 +51,57 @@ def main(args):
     cv2.imwrite(args.output, image)
 
 
+def main(args):
+    user_input = input("Enter a prompt, or press ESC to exit: ")
+    while user_input != chr(27):
+        args.prompt = user_input
+        run_prompt(args)
+        user_input = input("Enter another prompt, or press ESC to exit: ")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # pipeline configure
-    parser.add_argument("--model", type=str, default="bes-dev/stable-diffusion-v1-4-openvino", help="model name")
+    parser.add_argument(
+        "--model", type=str, default="bes-dev/stable-diffusion-v1-4-openvino", help="model name")
     # inference device
-    parser.add_argument("--device", type=str, default="CPU", help=f"inference device [{', '.join(Core().available_devices)}]")
+    parser.add_argument("--device", type=str, default="CPU",
+                        help=f"inference device [{', '.join(Core().available_devices)}]")
     # randomizer params
-    parser.add_argument("--seed", type=int, default=None, help="random seed for generating consistent images per prompt")
+    parser.add_argument("--seed", type=int, default=None,
+                        help="random seed for generating consistent images per prompt")
     # scheduler params
-    parser.add_argument("--beta-start", type=float, default=0.00085, help="LMSDiscreteScheduler::beta_start")
-    parser.add_argument("--beta-end", type=float, default=0.012, help="LMSDiscreteScheduler::beta_end")
-    parser.add_argument("--beta-schedule", type=str, default="scaled_linear", help="LMSDiscreteScheduler::beta_schedule")
+    parser.add_argument("--beta-start", type=float,
+                        default=0.00085, help="LMSDiscreteScheduler::beta_start")
+    parser.add_argument("--beta-end", type=float, default=0.012,
+                        help="LMSDiscreteScheduler::beta_end")
+    parser.add_argument("--beta-schedule", type=str, default="scaled_linear",
+                        help="LMSDiscreteScheduler::beta_schedule")
     # diffusion params
-    parser.add_argument("--num-inference-steps", type=int, default=32, help="num inference steps")
-    parser.add_argument("--guidance-scale", type=float, default=7.5, help="guidance scale")
+    parser.add_argument("--num-inference-steps", type=int,
+                        default=32, help="num inference steps")
+    parser.add_argument("--guidance-scale", type=float,
+                        default=7.5, help="guidance scale")
     parser.add_argument("--eta", type=float, default=0.0, help="eta")
     # tokenizer
-    parser.add_argument("--tokenizer", type=str, default="openai/clip-vit-large-patch14", help="tokenizer")
+    parser.add_argument("--tokenizer", type=str,
+                        default="openai/clip-vit-large-patch14", help="tokenizer")
     # prompt
-    parser.add_argument("--prompt", type=str, default="Street-art painting of Emilia Clarke in style of Banksy, photorealism", help="prompt")
+    parser.add_argument(
+        "--prompt", type=str, default="Street-art painting of Emilia Clarke in style of Banksy, photorealism", help="prompt")
     # Parameter re-use:
-    parser.add_argument("--params-from", type=str, required=False, help="Extract parameters from a previously generated image.")
+    parser.add_argument("--params-from", type=str, required=False,
+                        help="Extract parameters from a previously generated image.")
     # img2img params
-    parser.add_argument("--init-image", type=str, default=None, help="path to initial image")
-    parser.add_argument("--strength", type=float, default=0.5, help="how strong the initial image should be noised [0.0, 1.0]")
+    parser.add_argument("--init-image", type=str,
+                        default=None, help="path to initial image")
+    parser.add_argument("--strength", type=float, default=0.5,
+                        help="how strong the initial image should be noised [0.0, 1.0]")
     # inpainting
-    parser.add_argument("--mask", type=str, default=None, help="mask of the region to inpaint on the initial image")
+    parser.add_argument("--mask", type=str, default=None,
+                        help="mask of the region to inpaint on the initial image")
     # output name
-    parser.add_argument("--output", type=str, default="output.png", help="output image name")
+    parser.add_argument("--output", type=str,
+                        default="output.png", help="output image name")
     args = parser.parse_args()
     main(args)
--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-from python:3.8-bookworm
+from python:3.8-slim-buster
 
 # Set the working directory to /app
 WORKDIR /.
@@ -6,10 +6,14 @@ WORKDIR /.
 # Copy the current directory contents into the container at /.
 COPY . /.
 
-RUN pip install --no-cache-dir "openvino-dev[onnx,pytorch]==2022.3.0"
+RUN apt update && apt install -y libsm6 libxext6 ffmpeg libfontconfig1 libxrender1 libgl1-mesa-glx
 
+RUN python -m pip install --upgrade pip
+
+RUN pip install  "openvino-dev[onnx,pytorch]==2022.3.0"
 # Run the command to install any necessary dependencies
 RUN pip install --no-cache-dir -r requirements.txt
+
 
 # Run hello.py when the container launches
 ENTRYPOINT ["python","./demo.py"]
